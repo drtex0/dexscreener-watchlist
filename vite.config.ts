754c852b3bea5feb8c2266419f47eddd5621b8d9
0xTex <tdza@mail.co>
2022-08-12 11:02:18 +0200

    chore: better build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'public/index.html'),
+    },
+  },
 });
