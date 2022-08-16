b9ff334eeaeadc5a47465f0e47d1580aa5b2c4b9
0xTex <tdza@mail.co>
2022-08-16 23:46:44 +0200

    feat: add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html'),
-    },
+    sourcemap: true,
   },
 });
